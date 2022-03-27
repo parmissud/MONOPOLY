e3fee00944cf3eb7df629d1a69deb730576e007c
SetarehPishiyar <pishiyarsetareh@gmail.com>
2022-03-27 17:37:22 +0430

    Bank.java updated

---
+++ Bank.java
@@ -0,0 +1,43 @@
+package MONOPOLY;
+
+import java.util.Scanner;
+
+public class Bank extends Square{
+
+    public Bank(String name, Player currentPlayer){
+        super(name);
+    }
+    public Bank(){}
+
+    public void offerInvest(Player currentPlayer){
+        System.out.println("Do you want to invest? y/n");
+        Scanner input = new Scanner(System.in);
+        String answer = input.next();
+
+        while (answer.equalsIgnoreCase("y") && answer.equalsIgnoreCase("n")){
+            System.out.println("invalid input. y/n");
+            answer = input.next();
+        }
+
+        if(answer.equalsIgnoreCase("y")) {
+            invest(currentPlayer);
+            currentPlayer.invested = true;
+        }
+    }
+
+    public void invest(Player currentPlayer){
+        currentPlayer.lendedMoney = currentPlayer.money / 2;
+        currentPlayer.money -= currentPlayer.lendedMoney;
+    }
+
+    public void getBonus(Player currentPlayer){
+
+        if(currentPlayer.invested){
+            currentPlayer.money += currentPlayer.lendedMoney*2;
+            currentPlayer.lendedMoney = 0;
+            currentPlayer.invested = false;
+        }
+        else
+            System.out.println("You have no investment.");
+    }
+}
