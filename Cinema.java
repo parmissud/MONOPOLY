b09ff476a2974bc4d76770df33b896b4ed37a952
Melika Eghbal <meleghbal@gmail.com>
2022-04-05 17:30:04 +0430

    Cinema, House and Hotel Updated.

--- Cinema.java
+++ Cinema.java
@@ -1,16 +1,13 @@
 package MONOPOLY;
 public class Cinema extends Property {
-    public Cinema(String name, int price , int rent) {
-        super(name, 200, 25);
+    public Cinema(int index) {
+        super(index, 200, 25);
     }
 
     //when owner buys a new cinema ( more than one ) this method should be called
-    public void increaseRent() {
+    public void increaseRent(Player currentPlayer) {
+        if(currentPlayer.getNumCinemas() == 1)
+            return;
         setRent(getRent()*2);
-    }
-
-    public void buyTicket(Player currentPlayer){
-        currentPlayer.money -= getRent();
-        System.out.println("Ticket Bought. Enjoy!");
     }
 }
--- Hotel.java
+++ Hotel.java
@@ -1,7 +1,7 @@
 package MONOPOLY;
 
 public class Hotel extends Property {
-    public Hotel(String name, int price, int rent) {
-        super(name, price, 600);
+    public Hotel(int index, int price) {
+        super(index, price, 600);
     }
 }
