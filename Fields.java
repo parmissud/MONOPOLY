3d81f27099b265703ea57ec7462f70deb15f98aa
Melika Eghbal <meleghbal@gmail.com>
2022-04-05 17:44:39 +0430

    Some methods are fixed and added to Fields.java

--- Fields.java
+++ Fields.java
@@ -6,34 +6,51 @@ public class Fields extends Property {
     private House[] houses = new House[4];
     private Hotel hotel;
 
-    public Fields(String name, int price, int rent) {
-        super(name, 100, rent);
+    public Fields(int index, int price, int rent) {
+        super(index, 100, rent);
     }
 
-    //when owner builds a new house ( more than one ) this method should be called
+    public int getNumHouses(){
+        int count=0;
+        for(int i=0; houses[i] != null; i++)
+            count++;
+        return count;
+    }
+
+    public int getNumHotels(){
+        if(hotel == null)
+            return 0;
+        return 1;
+    }
+
+    //when owner builds a new house ( more than one ) this method should be called.
     public void increaseRent() {
+        if(getNumHouses() == 1)
+            return;
         setRent(getRent() + 100);
     }
 
     //when owner builds a new house ( more than one ) or hotel this method should be called.
-    //this method returns the new price
-    public int increasePrice() {
-        return getPrice() + 150;
+    public void increasePrice() {
+        if(getNumHouses() == 0)
+            return;
+        setPrice(getPrice() + 150);
     }
 
-/*    private void setPrice() {
-        if(hotel != null) {
-            price = 800;
-            setRent(600);
-            return;
+    public void build(Player currentPlayer) {
+        if(getNumHouses() < 4) {
+            houses[getNumHouses()] = new House(index);
+            currentPlayer.addMoney(-150);
+            increaseRent();
+            increasePrice();
+            System.out.printf("Your House is built now! Now you have %d houses.\n", getNumHouses());
+        } else if(getNumHouses() == 4 && getNumHotels() == 0) {
+            hotel = new Hotel(index, getPrice()+100);
+            currentPlayer.addMoney(-100);
+            increasePrice();
+            System.out.println("Your hotel is built now!");
+        } else if(getNumHouses() == 4 && getNumHotels() == 1) {
+            System.out.println("You can't add any house or hotel in your field!");
         }
-        int len=0;
-        for(int i=0; houses[i] != null; i++)
-            len++;
-        price = 100 + len*150;
-        setRent(50 + len*100);
-        //----------
-        // CINEMA
-        //---------
-    }*/
+    }
 }
